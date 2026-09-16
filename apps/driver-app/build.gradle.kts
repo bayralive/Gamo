d3b15b1f52e653c728ccb8cbcb1dd396f0b56c27
bayralive <director@bayratravel.com>
2026-09-16 10:49:44 +0300

    Set Driver App to Version 6 (versionCode 6)

--- apps/driver-app/build.gradle.kts
+++ apps/driver-app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.bayra.driver"
         minSdk = 24
         targetSdk = 36
-        versionCode = 21
-        versionName = "2.31.21"
+        versionCode = 6
+        versionName = "2.31.6"
     }
 
     buildTypes {
